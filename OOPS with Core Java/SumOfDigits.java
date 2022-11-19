9d4304a520bde83478bb6a231cd38834998db6a2
Santhosh S <sureshsanthosh017@gmail.com>
2022-11-19 18:37:43 +0530

    mistakes solved

--- OOPS with Core Java/SumOfDigits.java
+++ OOPS with Core Java/SumOfDigits.java
@@ -1,3 +1,30 @@
+/*
+Write a program to implement the following logic using inheritance.
+
+Create a parent class and implement the fun method. In the method,
+get the individual digits of the entered number,
+store it in an array, and find their sum. 
+For example in case of 1234, the individual digits are 4,3,2,1 
+and the final sum → (4+3)+(4+2)+(4+1)+(3+2)+(3+1)+(2+1) = 30. 
+Create the main class that inherits the parent class and 
+call the fun method inside the parent function.
+
+Input format
+The input consists of an integer.
+
+Output format
+The output prints the final sum.
+
+Code constraints
+Integers only.
+
+Sample testcases
+Input 1           Output 1
+1234              30
+
+Input 2           Output 2
+4356              54
+*/
 import java.util.*;
 class SumOfDigits{
     void fun(int[] a,int n,int len){
